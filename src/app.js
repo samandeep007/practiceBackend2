b869f40b16b6f33c32f3e8a648bbd21369a08d0f
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-29 13:59:48 -0600

    chore: Add express rate limiting and request parsing

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import cookieParser from 'cookie-parser';
+import express, { urlencoded } from'express';
+import rateLimit from 'express-rate-limit';
+
+const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: "Too many requests from this IP. Try again later!",
+    header: true
+})
+
+app.use(urlencoded({
+    limit: "16kb",
+    extended: true
+}));
+
+app.use(express.json({
+    limit: "16kb"
+}));
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credentials: true
+}))
+
+app.use(cookieParser());
+
+app.use(limiter);
+
+export {app};
